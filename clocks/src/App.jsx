7c8eb0a59c840ef51250b0619cde23c5bee613a2
aashi <aashiemail02@gmail.com>
2025-05-21 18:17:34 +0530

    initial commit

---
+++ clocks/src/App.jsx
@@ -0,0 +1,17 @@
+
+import DigitalClock from "./DigitalClock.jsx";
+import StopWatch from "./StopWatch.jsx";
+
+function App() {
+  
+  return (
+    <>
+      <DigitalClock/>
+      <StopWatch/>
+    </>
+    
+    
+  );
+}
+
+export default App
