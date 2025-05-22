9008427b6f7a95188f36afc5fb51374f2f4d1433
aashi <aashiemail02@gmail.com>
2025-05-23 02:33:42 +0530

    create automated test for order summary

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,6 +1,11 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let  cart;
 
-if (!Array.isArray(cart) || cart.length === 0) {
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -17,6 +22,9 @@ if (!Array.isArray(cart) || cart.length === 0) {
 
   localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+}
+
 
 console.log(cart);
 
