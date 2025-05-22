a24f82a7ebd4d70c4444a2c8c75c630d26928d03
aashi <aashiemail02@gmail.com>
2025-05-22 16:05:14 +0530

    save cart into local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,11 +1,22 @@
-export const cart=[{
+export let  cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    [{
     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2
+    quantity:2,
 },{
     productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity:1
 
 }];  
+
+}
+
+
+
+export function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
             let matchingItem;
@@ -26,6 +37,9 @@ export function addToCart(productId){
 
                 }
 
+
+                saveToStorage();
+
                 
 
         }
@@ -39,4 +53,6 @@ export function removeFromCart(productId){
         }
     });
     cart=newCart;
+
+    saveToStorage();
 }
