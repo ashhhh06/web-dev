094a527207597d710c197295d7b6251843cf76e5
aashi <aashiemail02@gmail.com>
2025-05-23 21:02:20 +0530

    learn promises and convert code to promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -4,9 +4,85 @@ import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js'; 
 
+new Promise((resolve)=>{
+    //console.log('start promise')
+    loadProducts(()=>{
+        //console.log('finished loading)
+        resolve(value1);
+    })
+}).then((value)=>{
+    console.log(value); 
+    //console.log('next step');
+    renderOrderSummary();
+    renderPaymentSummary();
+
+})
+
+
+/*
+
+Promise.all([
+    new Promise((resolve)=>{
+    
+    loadProducts(()=>{
+        resolve(value1);
+    })
+
+
+    }),
+
+
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+            
+
+        });
+    })
+
+
+
+]).then((value)=>{
+    console.log(value);
+    renderOrderSummary();
+    renderPaymentSummary(); 
+})
+
+o/p ==== [value1,undefined]
+
+
+
+
+
+
+
+new Promise((resolve)=>{
+    
+    loadProducts(()=>{
+        resolve();
+    })
+
+
+}).then(()=>{
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+            
+
+        });
+    })
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+*/
+
+//using callback
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 
 });
 
+*/
