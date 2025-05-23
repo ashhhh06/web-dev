51d4e76d6e6c7ecd3c16f79da781412a9b804b62
aashi <aashiemail02@gmail.com>
2025-05-23 16:19:05 +0530

    load products from the b=backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -111,8 +111,31 @@ const product1=new Product({
     ]
   },);
 
-console.log(product1);
+//console.log(product1);
 
+export let products=[];
+
+export function loadProducts(fun){
+  const xhr=new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    products=JSON.parse(xhr.response).map((productDetails)=>{
+
+        if(productDetails.type==='clothing'){
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+
+      });;
+      fun();
+    });
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -780,6 +803,7 @@ export const products = [
   return new Product(productDetails);
 
 });
+*/
 
 //now each object of product becomes a class 
 //array of objects is now converted to array of object product  classes
